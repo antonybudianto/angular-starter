da68ceccdf8b9b295d759d8c91b7b2cf57d44a2d
Antony Budianto <antonybudianto@gmail.com>
2015-12-26 18:17:35 +0700

    refactor unit test

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -2,17 +2,18 @@ import {
     it,
     describe,
     expect,
-    injectAsync,
+    inject,
     TestComponentBuilder
 } from 'angular2/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-    it('should have brand Angular 2', injectAsync([TestComponentBuilder], (tcb) => {
-        return tcb.createAsync(AppComponent).then((fixture) => {
+    it('should have brand Angular 2', inject([TestComponentBuilder], tcBuilder => {
+        tcBuilder.createAsync(AppComponent).then((fixture) => {
             fixture.detectChanges();
             let compiled = fixture.debugElement.nativeElement;
-            expect(compiled).toContainText('Angular 2 Starter');
+            console.log(compiled);
+            expect(compiled).toContainText('Angular 2 Stsarter');
         });
     }));
 });
--- app/blocks/logger.service.spec.ts
+++ app/blocks/logger.service.spec.ts
@@ -1,20 +1,22 @@
 import {
     it,
+    inject,
     describe,
     expect,
-    beforeEach
+    beforeEach,
+    beforeEachProviders
 } from 'angular2/testing';
 import { LoggerService } from './logger.service';
 
 describe('AppComponent', () => {
+    beforeEachProviders(() => [LoggerService]);
     beforeEach(() => {
         spyOn(console, 'log');
     });
 
-    it('should log successfully', () => {
+    it('should log successfully', inject([LoggerService], (logger) => {
         let message = 'Hello';
-        let logger = new LoggerService();
         logger.log(message);
         expect(console.log).toHaveBeenCalledWith(message);
-    });
+    }));
 });
