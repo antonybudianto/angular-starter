b7eeeb450f9b06f61c357a33f1abc626e9fe68b6
Antony <antony.budianto@gdplabs.id>
2015-12-29 10:54:05 +0700

    sass: compile only main sass file

--- tasks/sass.js
+++ tasks/sass.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var config = require('../gulp.config')();
  
 gulp.task('sass', function () {
-    return gulp.src(config.assetsPath.styles + '**/*.scss')
+    return gulp.src(config.assetsPath.styles + 'main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(config.assetsPath.styles));
 });
