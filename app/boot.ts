9ea300cbaa72bd1aacb4f307db2a3aa90f7f0c33
Antony Budianto <antonybudianto@gmail.com>
2016-02-14 20:17:38 +0700

    enable tsc without gulp-typescript

--- app/boot.ts
+++ app/boot.ts
@@ -1,3 +1,5 @@
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
+
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {AppComponent} from './app.component';
