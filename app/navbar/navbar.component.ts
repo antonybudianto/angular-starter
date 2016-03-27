f3dfb4fbf843530c201fa6c775217fb586800d5c
Antony Budianto <antonybudianto@gmail.com>
2016-03-27 21:31:49 +0700

    refactor(app): add changeDetection strategy

--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input} from 'angular2/core';
+import {Component, Input, ChangeDetectionStrategy} from 'angular2/core';
 import {CORE_DIRECTIVES} from 'angular2/common';
 import {RouterLink, RouteDefinition} from 'angular2/router';
 
 @Component({
     selector: 'navbar',
     templateUrl: 'app/navbar/navbar.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
     directives: [RouterLink, CORE_DIRECTIVES]
 })
 export class NavbarComponent {
