a653ef38fe4df43bf755cf2ead0f7e40b325e263
Antony Budianto <antonybudianto@gmail.com>
2016-05-04 07:52:22 +0700

    Drop live-server for Browsersync, update rc.1

--- tasks/serve.js
+++ tasks/serve.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
-var liveServer = require('live-server');
 var config = require('../gulp.config')();
+
+function startBrowsersync(config) {
+    var bs = require("browser-sync").create();
+    bs.init(config);
+    bs.reload();
+}
 
 /* Start live server dev mode */
 gulp.task('serve-dev', ['sass', 'tsc-app', 'watch-ts', 'watch-sass'], function () {
-    liveServer.start(config.liveServer.dev);
+    startBrowsersync(config.browserSync.dev);
 });
 
 /* Start live server production mode */
 gulp.task('serve-build', ['build'], function () {
-    liveServer.start(config.liveServer.prod);
+    startBrowsersync(config.browserSync.prod);
 });
