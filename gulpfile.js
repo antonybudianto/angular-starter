aef9e1a2488bd10782975251083694b856758882
Darius Laurinčikas <darx21@gmail.com>
2016-06-07 20:04:54 +0300

    set default environment to dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-var gulp = require('gulp'),
+var argv = require('yargs').argv,
+    gulp = require('gulp'),
     requireDir = require('require-dir'),
     tasks = requireDir('./tasks');
 
+global.env = argv.env || 'dev';
+console.log(global.env);
 var config = require('./gulp.config')();
 
 /* Default task */
