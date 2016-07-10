1d41eaf7625b883c99984a006549873d1de9a7ea
Antony Budianto <antonybudianto@gmail.com>
2016-07-10 20:55:55 +0700

    chore(package.json): update deps and new gulp-tslint api

--- config/gulp/tasks/typescript.js
+++ config/gulp/tasks/typescript.js
@@ -58,10 +58,10 @@ gulp.task('tslint-e2e', function () {
 
 function lintTs(files) {
     return gulp.src(files)
-        .pipe(tslint())
-        .pipe(tslint.report('prose', {
-          summarizeFailureOutput: true
-        }));
+        .pipe(tslint({
+            formatter: 'verbose'
+        }))
+        .pipe(tslint.report());
 }
 
 function compileTs(files, watchMode) {
@@ -74,11 +74,10 @@ function compileTs(files, watchMode) {
             base: config.src,
             outDir: config.tmp
         })
-        .pipe(tslint())
-        .pipe(tslint.report('prose', {
-            summarizeFailureOutput: true,
-            emitError: !watchMode
+        .pipe(tslint({
+            formatter: 'verbose'
         }))
+        .pipe(tslint.report())
         .pipe(sourcemaps.init())
         .pipe(ts(tsProject))
         .on('error', function () {
