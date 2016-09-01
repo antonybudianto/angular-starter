5d0f1f25dde9ebb7121d97fe28af6e3a63f1f060
Antony <antony.budianto@gdplabs.id>
2016-09-01 13:40:53 +0700

    fix systemjs config for new rc6 bundles

--- config/test/karma.conf.js
+++ config/test/karma.conf.js
@@ -30,6 +30,10 @@ module.exports = function(config) {
     files: [
       'node_modules/core-js/client/shim.min.js',
       'node_modules/zone.js/dist/zone.js',
+      'node_modules/zone.js/dist/long-stack-trace-zone.js',
+      'node_modules/zone.js/dist/proxy.js',
+      'node_modules/zone.js/dist/sync-test.js',
+      'node_modules/zone.js/dist/jasmine-patch.js',
       'node_modules/zone.js/dist/async-test.js',
       'node_modules/zone.js/dist/fake-async-test.js',
       'node_modules/systemjs/dist/system.src.js'
